a467e5076ef6611252833e9c05487b0142cec7f6
Nandhitha R <NandhithaR@users.noreply.github.com>
2020-04-30 21:28:51 -0400

    Add files via upload

--- project.scala
+++ project.scala
@@ -145,6 +145,41 @@ def popularKeywords(): DataFrame = {
 
 val popularKeywordsdf = popularKeywords()
 
+//Number of publications from paper venue
+def NumberOfPublications_ByVenue(): DataFrame = {
+    val NumberOfPublications_ByVenue = ResearchPaperDF.groupBy("venue").count()
+    NumberOfPublications_ByVenue
+}
+val NumberOfPublications_ByVenue_DF = NumberOfPublications_ByVenue()
+
+
+// Number of publications in different languages
+def NumberOfPublications_ByLanguage(): DataFrame = {
+    val NumberOfPublications_ByLanguage = ResearchPaperDF.groupBy("lang").count()
+    NumberOfPublications_ByLanguage
+}
+val NumberOfPublications_ByLanguage_DF = NumberOfPublications_ByLanguage()
+
+//Group publication by their volume
+def volume_Of_Paper(): DataFrame = {
+    val volume_Of_Paper = ResearchPaperDF.withColumn("Volume_ofPaper",$"page_end" - $"page_start")
+    val volume_Of_Paper_df = volume_Of_Paper.groupBy($"Volume_ofPaper").count()
+    volume_Of_Paper_df
+}
+
+val volume_Of_Paper_DF = volume_Of_Paper()
+
+
+//Group publication by their volume
+def volume_Of_Paper_fos(): DataFrame = {
+    val volume_Of_Paper_fos = ResearchPaperDF.withColumn("Volume_ofPaper",$"page_end" - $"page_start")
+    volume_Of_Paper_fos = volume_Of_Paper_fos.groupBy($"lang").mean("Volume_ofPaper")
+    volume_Of_Paper_fos
+}
+
+val volume_Of_Paper_fos = volume_Of_Paper_fos()
+
+
 //page rank for the authors in CS research papers
 def pageRankAuthor() ={
     val authorstCS = ResearchPaperDF.select("id","authors_name","fos").where(ResearchPaperDF("fos")==="Computer Science")
@@ -170,22 +205,107 @@ def pageRankAuthor() ={
 var authorRanks = pageRankAuthor()
 
 def createAuthorsGraph()={
-    val distinctMagDF = magDF.distinct()
-    val magSelectColumns= distinctMagDF.select("authors","fos","id","keywords","lang","n_citation","publisher","references","title","venue","year")
-    val magDropNA = magSelectColumns.na.drop()
-    val authorDetails = magDropNA.select("id","authors","fos")
-    val authorsDetailsDf = authorDetails.withColumn("authors_name", magDropNA("authors.name"))
-    val authorsExplodefos = authorsDetailsDf.select($"id",$"authors_name",explode($"fos").alias("fos"))
-    val authorDf = authorsExplodefos.select($"id",explode($"authors_name").alias("authors"),$"fos").na.drop().distinct()
-    val df1 = authorDf.withColumnRenamed("id","id_1").withColumnRenamed("authors","authors1").withColumnRenamed("fos","fos1")
-    val df2 = authorDf.withColumnRenamed("id","id_2").withColumnRenamed("authors","authors2").withColumnRenamed("fos","fos2")
-    val df_authors = df1.join(df2, ((df1("id_1")===df2("id_2")) && (df1("authors1")!==df2("authors2"))),"inner").na.drop().distinct()
-    val df =  df_authors.select("authors1","authors2","fos1")
-    val vertices = df.select("authors1").distinct().na.drop()
+    val researchDF = ResearchPaperDF.select("id","authors_name","fos").na.drop()
+    val researchDF1 = researchDF.withColumnRenamed("id","id1").withColumnRenamed("authors_name","authors1").withColumnRenamed("fos","fos1")
+    val researchDF2 = researchDF.withColumnRenamed("id","id2").withColumnRenamed("authors_name","authors2").withColumnRenamed("fos","fos2")
+    val df_authors = researchDF1.join(researchDF2, ((researchDF1("id1")===researchDF2("id2")) && (researchDF1("authors1")!==researchDF2("authors2"))),"inner").na.drop().distinct()
+    val authorsDf =  df_authors.select("authors1","authors2","fos1")
+    val vertices = authorsDf.select("authors1").distinct().na.drop()
     val v = vertices.withColumnRenamed("authors1","id")
-    val e = df.withColumnRenamed("authors1","src").withColumnRenamed("authors2","dst").withColumnRenamed("fos1","relationship")
+    val e = authorsDf.withColumnRenamed("authors1","src").withColumnRenamed("authors2","dst").withColumnRenamed("fos1","relationship")
     val g = GraphFrame(v, e)
     g
 }
 
-val g = createAuthorsGraph()
+val g_author_fos = createAuthorsGraph()
+
+//Queries on the graph
+val results = g.triangleCount.run()
+results.select("id", "count").show()
+
+val result = g.labelPropagation.maxIter(5).run()
+result.select("id", "label").show()
+
+val numField = g.edges.filter("relationship = 'Economics' AND relationship = 'Marketing'").count()
+val numField = g.edges.filter("relationship = 'Economics' ").count()
+
+// Graph of Volume of paper's published to year
+def year_VolumeOfPapers_Graph()={
+    var vertexArray = ResearchPaperDF.select("year")
+    vertexArray = vertexArray.na.drop()
+    vertexArray = vertexArray.distinct()
+    vertexArray = vertexArray.withColumnRenamed("year","year_publish")
+    var edge_Array = ResearchPaperDF.groupBy("year").count()
+    val graph_data = vertexArray.join(edge_Array, ((vertexArray("year_publish")===edge_Array("year"))),"inner")
+    var edges = graph_data.select($"year",$"count")
+    var vertex = graph_data("year_publish")
+    var vertex_withid = vertex.withColumnRenamed("year_publish","id")
+    var edges_withReln = graph_data.withColumnRenamed("year_publish","src").withColumnRenamed("year","dst").withColumnRenamed("count","relationship")
+    var graph_year_volumeOfPublish = GraphFrame(vertex_withid, edges_withReln)
+    graph_year_volumeOfPublish
+}
+
+val volumeOfPaper_year = year_VolumeOfPapers_Graph()
+
+
+//Keywords analysis with papers and authors 
+def createKeywordsGraph() = {
+    val researchDF = ResearchPaperDF.select("id","authors_name","title","keywords","year","n_citation").na.drop()
+    val researchDF1 = researchDF.withColumnRenamed("id","id1").withColumnRenamed("authors_name","authors1").withColumnRenamed("keywords","keywords1").withColumnRenamed("year","year1").withColumnRenamed("n_citation","n_citation1").withColumnRenamed("title","title1")
+    val researchDF2 = researchDF.withColumnRenamed("id","id2").withColumnRenamed("authors_name","authors2").withColumnRenamed("keywords","keywords2").withColumnRenamed("year","year2").withColumnRenamed("n_citation","n_citation2").withColumnRenamed("title","title2")
+    val df_keywords = researchDF1.join(researchDF2, ((researchDF1("id1")===researchDF2("id2")) && (researchDF1("authors1")!==researchDF2("authors2"))),"inner").na.drop().distinct()
+    //show df_keywords
+    val authorsDf =  df_keywords.select("authors1","authors2","keywords1","title1","year1","n_citation1")
+    val vertices = authorsDf.select("authors1","title1","year1","n_citation1").distinct().na.drop()
+    val v = vertices.withColumnRenamed("authors1","id")
+    val e = authorsDf.withColumnRenamed("authors1","src").withColumnRenamed("authors2","dst").withColumnRenamed("keywords1","relationship")
+    val g = GraphFrame(v, e)
+    g
+}
+
+val g_author_keyword = createKeywordsGraph()
+
+def createFieldOfStudyGraph() = {
+    val researchDF = ResearchPaperDF.select("id","authors_name","title","keywords","year","n_citation","fos").na.drop()
+    val researchDF1 = researchDF.withColumnRenamed("id","id1").withColumnRenamed("authors_name","authors1").withColumnRenamed("keywords","keywords1").withColumnRenamed("year","year1").withColumnRenamed("n_citation","n_citation1").withColumnRenamed("title","title1").withColumnRenamed("fos","fos1")
+    val researchDF2 = researchDF.withColumnRenamed("id","id2").withColumnRenamed("authors_name","authors2").withColumnRenamed("keywords","keywords2").withColumnRenamed("year","year2").withColumnRenamed("n_citation","n_citation2").withColumnRenamed("title","title2").withColumnRenamed("fos","fos2")
+    val df_keywords = researchDF1.join(researchDF2, ((researchDF1("id1")===researchDF2("id2")) && (researchDF1("keywords1")!==researchDF2("keywords2"))),"inner").na.drop().distinct()
+    //show df_keywords
+    val keywordsDf =  df_keywords.select("keywords1","keywords2","authors1","title1","year1","n_citation1","fos1")
+    val vertices = keywordsDf.select("keywords1","title1","year1","n_citation1","authors1").distinct().na.drop()
+    val v = vertices.withColumnRenamed("keywords1","id")
+    val e = keywordsDf.withColumnRenamed("keywords1","src").withColumnRenamed("keywords2","dst").withColumnRenamed("fos1","relationship")
+    val g = GraphFrame(v, e)
+    g
+}
+
+val g_keyword_fos = createFieldOfStudyGraph()
+
+def createPublisherGraph() = {
+    val researchDF = ResearchPaperDF.select("id","keywords","publisher","fos").where(ResearchPaperDF("fos")==="Computer Science").na.drop()
+    val researchDF1 = researchDF.withColumnRenamed("id","id1").withColumnRenamed("publisher","publisher1").withColumnRenamed("keywords","keywords1").withColumnRenamed("fos","fos1")
+    val researchDF2 = researchDF.withColumnRenamed("id","id2").withColumnRenamed("publisher","publisher2").withColumnRenamed("keywords","keywords2").withColumnRenamed("fos","fos2")
+    val df_publisher = researchDF1.join(researchDF2, ((researchDF1("id1")===researchDF2("id2")) && (researchDF1("keywords1")!==researchDF2("keywords2"))),"inner").na.drop().distinct()
+    //show df_keywords
+    val publisherDf =  df_publisher.select("keywords1","keywords2","publisher1")
+    val vertices = publisherDf.select("keywords1").distinct().na.drop()
+    val v = vertices.withColumnRenamed("keywords1","id")
+    val e = publisherDf.withColumnRenamed("keywords1","src").withColumnRenamed("keywords2","dst").withColumnRenamed("publisher1","relationship")
+    val g = GraphFrame(v, e)
+    g
+}
+
+val g_keyword_publisher = createPublisherGraph()
+
+
+g_author_fos.vertices.write.parquet("user/nr2229/Project/Outputs/Grpahs/authorfos/vertices")
+g_author_fos.edges.write.parquet("user/nr2229/Project/Outputs/Grpahs/authorfos/edges")
+
+g_author_keyword.vertices.write.parquet("user/nr2229/Project/Outputs/Grpahs/authorkeywords/vertices")
+g_author_keyword.edges.write.parquet("user/nr2229/Project/Outputs/Grpahs/authorkeywords/edges")
+
+g_keyword_publisher.vertices.write.parquet("user/nr2229/Project/Outputs/Grpahs/keywordpublisher/vertices")
+g_keyword_publisher.edges.write.parquet("user/nr2229/Project/Outputs/Grpahs/keywordpublisher/edges")
+
+g_keyword_fos.vertices.write.parquet("user/nr2229/Project/Outputs/Grpahs/keywordfos/vertices")
+g_keyword_fos.edges.write.parquet("user/nr2229/Project/Outputs/Grpahs/keywordfos/edges")
